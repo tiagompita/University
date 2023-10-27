d88602003b036616f4f7d303f481c6fbb1cd6f5e
BravooZ <tiagomsp9@gmail.com>
2023-10-27 15:37:41 +0100

    Projeto 1

--- 1 ano/FP/Projeto 1/forca.py
+++ 1 ano/FP/Projeto 1/forca.py
@@ -3,6 +3,13 @@ AUTORES = [120152, 119928]
 
 import random
 
+################ Variáveis #################
+#Corpo do boneco
+mostrar = ["O", "\\", "X", "/", "|", "/", "\\"]
+
+#Lista para ocultar o corpo do boneco
+oculto = [" "," "," "," "," "," "," ",]
+
 
 # Defina funções aqui.
 
@@ -10,18 +17,20 @@ import random
 def enforcado(valores):
 
     print()
-    print("\t  +-------+", sep="\t")
-    print("\t  | ","| |", sep="\t")
-    print("\t  {}".format(valores[0]),"| |", sep="\t")
-    print("\t{} {} {}".format(valores[1], valores[2], valores[3]), "| |", sep="\t")
-    print("\t  {}".format(valores[4]), "| |", sep="\t")
-    print("\t {} {}".format(valores[5], valores[6]), "| |", sep="\t")
-    print("","", "| |", sep="\t")
+    print("\t  +-------+")
+    print("\t  | ","\t| |")
+    print("\t  {}".format(valores[0]),"\t| |")
+    print("\t{} {} {}".format(valores[1], valores[2], valores[3]), "\t| |")
+    print("\t  {}".format(valores[4]), "\t| |")
+    print("\t {} {}".format(valores[5], valores[6]), "\t| |")
+    print("","\t", "\t| |")
     print(" _______________|_|___")
     print(" `````````````````````")
     print()
+    print()
 
-def palavraSecreta():
+
+def main():
     from wordlist import words1, words2
     
     # Descomente a linha que interessar para testar
@@ -31,52 +40,76 @@ def palavraSecreta():
    
     # Escolhe palavra aleatoriamente
     secret = random.choice(words).upper()
+    # Cria uma lista que contém os espaços em branco para cada letra da palavra
+    espacos = ["_ "] * len(secret)
 
-    #Devolve a palavra
-    return secret
-
-
-def main():
     # Complete o programa
-    print("Olá, bem-vindo ao jogo da forca!")
-    print("Tens {} tentativas para adivinhar a palavra.".format(len(palavraSecreta()) + 2))
+
+    print("\nOlá, bem-vindo ao jogo da forca!")
+    print("Tens 7 tentativas para adivinhar a palavra.")
 
 
-#Corpo do boneco
-mostrar = ["O", "\\", "X", "/", "|", "/", "\\"]
+    #O numero de tentativas (apenas 7 tentativas, pois o corpo apenas tem 7 partes)
+    ntentativas = 7
 
-#Lista para ocultar o corpo do boneco
-oculto = [" "," "," "," "," "," "," ",]
+    #Letras adivinhadas
+    guessedCorrect = []
+    guessedWrong = []
 
-#O numero de tentativas (apenas 7 tentativas, pois o corpo apenas tem 7 partes)
-ntentativas = 7
+    while ntentativas > 0 and "_ " in espacos:
 
-#Letras adivinhadas
-guessedCorrect = []
-guessedWrong = []
+        
+        LetCerta = "Letras Certas: " + " - ".join(guessedCorrect).upper()
+        LetErr = "Letras Erradas: " + " - ".join(guessedWrong).upper()
 
-# Cria uma lista que contém os espaços em branco para cada letra da palavra
-espacos = ["_ "] * len(palavraSecreta())
+        enforcado(oculto)
 
-while ntentativas != 0 and "_" in espacos:
-
-    #Pede para digitar uma letra
-    letra = input("Digite uma letra: ").lower().isalpha()
-
-    #Se a letra ja foi adivinhada:
-    if letra in guessedCorrect or guessedWrong:
-        print("Já adivinhaste essa letra")
-    
-    if letra in palavraSecreta():
-        guessedCorrect += letra
-    else:
-        guessedWrong += letra
-        ntentativas -= 1
-
-
+        print("".join(espacos))
+        print()
+        print(LetCerta)
+        print(LetErr)
+        print("Tentativas: ", ntentativas)
 
         
 
+        #Pede para digitar uma letra
+        letra = str(input("\nIntroduz uma letra: ").upper())
+
+
+        #Verifica se não é digito, se a letra pertence ao alfabeto e é apenas 1 letra
+        if letra.isalpha() and not letra.isdigit() and len(letra) == 1:
+            #print("Letra válida")
+
+            #Se a letra ja foi adivinhada:
+            if letra in guessedCorrect or letra in guessedWrong:
+                print("*********\tJá adivinhaste essa letra\t*********")
+            else:
+                if letra in secret:
+                    guessedCorrect += letra
+                    # Substitui os espaços corretos com a letra adivinhada corretamente
+                    for i in range(len(secret)):
+                        if secret[i] == letra:
+                            espacos[i] = letra + " "
+                else:
+                    guessedWrong += letra
+                    ntentativas -= 1
+
+                    if 1 <= ntentativas <= 6:
+                        oculto[0:7-ntentativas] = mostrar[0:7-ntentativas]
+                        enforcado(oculto)
+                
+        elif letra == "":
+            print("\t\t********Letra inválida********")
+        else:
+            print("\t\t********Letra inválida********")
+    else:
+        if ntentativas == 0:
+            print("Fim do Jogo. Acabou-se as tentativas. A palavra era: {}". format(secret))
+            oculto[0:7] = mostrar[0:7]
+            enforcado(oculto)
+        if "_ " not in espacos:
+            print("Parabéns! Acertaste a palavra ( {} ).". format(secret))
+            
 
 if __name__ == "__main__":
     main()
---
+++ 1 ano/FP/Projeto 1/teste.py
@@ -0,0 +1,4 @@
+mostrar = ["O", "\\", "X", "/", "|", "/", "\\"]
+oculto = [" "," "," "," "," "," "," ",]
+oculto[0:1] = mostrar[0:1]
+print(mostrar, oculto)
