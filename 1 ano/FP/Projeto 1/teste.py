ab2d1abe0218620050eb425308a0e72248ffc015
BravooZ <tiagomsp9@gmail.com>
2023-11-02 16:06:12 +0000

    Cenas

--- 1 ano/FP/Projeto 1/teste.py
+++ 1 ano/FP/Projeto 1/teste.py
@@ -1,13 +1,8 @@
-import unicodedata
+#Corpo do boneco
+corpoInt = ["O", "/", "X", "\\", "|", "/", "\\"]
 
-def compare_strs(s1, s2):
-    def NFD(s):
-        return unicodedata.normalize('NFD', s)
-
-    return NFD(s1) == NFD(s2)
-
-single_char = 'ê'
-multiple_chars = '\N{LATIN SMALL LETTER E}\N{COMBINING CIRCUMFLEX ACCENT}'
-print('length of first string=', len(single_char))
-print('length of second string=', len(multiple_chars))
-print(compare_strs(single_char, multiple_chars))
+#Lista para ocultar o corpo do boneco
+corpo = [" "," "," "," "," "," "," ",]
+print(corpo)
+corpo[0:1]=corpoInt[0:1]
+print(corpo)
