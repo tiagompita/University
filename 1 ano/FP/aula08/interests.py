d1d0fd12d3a13c1bb23f245d8937a28b3166d59c
BravooZ <tiagomsp9@gmail.com>
2023-11-23 13:46:46 +0000

    IEI

--- 1 ano/FP/aula08/interests.py
+++ 1 ano/FP/aula08/interests.py
@@ -20,19 +20,36 @@ def main():
 
 
     print("a) Table of common interests:")
-    commoninterests = ...
-    print(commoninterests)
+    common_interests = {}
+
+    #Escolher duas pessoas
+    for person1 in interests:
+        for person2 in interests:
+            #Ser diferente e nao estar incluido
+            if person1 != person2 and (person2, person1) not in common_interests:
+                
+                common = interests[person1].intersection(interests[person2])
+                
+                common_interests[(person1, person2)] = common
+
+    print(common_interests)
 
     print("b) Maximum number of common interests:")
-    maxCI = ...
+    maxCI = max(len(interests) for interests in common_interests.values())
     print(maxCI)
 
     print("c) Pairs with maximum number of matching interests:")
-    maxmatches = ...
+    maxmatches = [pair for pair, interests in common_interests.items() if len(interests) == maxCI]
     print(maxmatches)
 
     print("d) Pairs with low similarity:")
-    lowJaccard = ...
+    lowJaccard = []
+    for pair, common in common_interests.items():
+        total = len(interests[pair[0]]) + len(interests[pair[1]]) - len(common)
+        jaccard = len(common) / total
+        if jaccard < 0.25:
+            lowJaccard.append(pair)
+
     print(lowJaccard)
 
 
