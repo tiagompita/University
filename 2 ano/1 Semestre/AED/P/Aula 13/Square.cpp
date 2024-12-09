96d81fe7181e72965acf4592bf3fc438bd9e524f
BravooZ <tiagomsp9@gmail.com>
2024-12-09 12:51:58 +0000

    AED e RC

---
+++ 2 ano/1 Semestre/AED/P/Aula 13/Square.cpp
@@ -0,0 +1,23 @@
+//
+// Algoritmos e Estruturas de Dados - 2024/2025
+//
+// J. Madeira - April/May 2022
+//
+// COMPLETE the code, according to Square.h
+//
+
+#include "Square.h"
+
+#include <cassert>
+#include <iostream>
+#include <string>
+
+#include "Point.h"
+#include "Rectangle.h"
+
+Square::Square(void) {
+  // Rectangle of edge=1 and centered at (0,0)
+  // COMPLETE
+}
+
+// WRITE THE MISSING CONSTRUTORS, METHODS AND FRIEND FUNCTIONS
---
+++ 2 ano/1 Semestre/AED/TP/24_AED_Int_C++_II/01_Fraction_Class/Fraction.h
@@ -0,0 +1,48 @@
+//
+// Algoritmos e Estruturas de Dados --- 2024/2025
+//
+// J. Madeira --- February 2022, March 2023
+//
+
+#ifndef FRACTION_H_
+#define FRACTION_H_
+
+#include <iostream>
+
+class Fraction {
+ public:
+  Fraction(void);
+  Fraction(int numerator, int denominator = 1);
+
+  int GetNumerator(void) const;
+  void SetNumerator(int n);
+
+  int GetDenominator(void) const;
+  void SetDenominator(int n);
+
+  // Comparison operators
+  bool operator==(const Fraction& frac) const;
+  bool operator!=(const Fraction& frac) const;
+  bool operator<(const Fraction& frac) const;
+
+  // Unary operator
+  Fraction operator-(void) const;
+
+  // Binary operators
+  Fraction operator+(const Fraction& frac) const;
+  Fraction operator-(const Fraction& frac) const;
+  Fraction operator*(const Fraction& frac) const;
+  Fraction operator/(const Fraction& frac) const;
+
+  double ToDouble(void) const;
+
+  friend std::ostream& operator<<(std::ostream& out, const Fraction& frac);
+
+ private:
+  void Reduce(void);
+
+  int numerator_;
+  int denominator_;  // ALWAYS POSITIVE !!!
+};
+
+#endif  // FRACTION_H_
