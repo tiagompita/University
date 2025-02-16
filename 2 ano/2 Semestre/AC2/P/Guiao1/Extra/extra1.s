fc001321e15fff4c914c25ebaaecff5539688a1f
pita <tiagomsp9@gmail.com>
2025-02-16 19:45:15 +0000

    AC2 - Conclusão Guiao 1

--- 2 ano/2 Semestre/AC2/P/Guiao1/Extra/extra1.s
+++ 2 ano/2 Semestre/AC2/P/Guiao1/Extra/extra1.s
@@ -1,8 +1,9 @@
 # Mapa de registos
 #
-#   state = $t0
-#   cnt =   $t1
-#   c =     $t2
+#   state = $s0
+#   cnt =   $s1
+#   c =     $s2
+#   temp = $t0
 
     .equ UP, 1
     .equ DOWN, 0
@@ -12,5 +13,84 @@
     .globl main
 
 main:
-    li      $t0, 0          $
-    li      $t1, 0
+    addi    $sp, $sp, -16
+    sw      $s0, 0($sp)
+    sw      $s1, 4($sp)
+    sw      $s2, 8($sp)
+    sw      $ra, 12($sp)
+
+    li      $s0, 0          # int state = 0
+    li      $s1, 0          # int cnt = 0
+
+do: li      $v0, 3          #
+    li      $a0, '\r'       #
+    syscall                 # putChar('\r') // Carriage return character
+
+    li      $v0, 6          #
+    move    $a0, $s1        #
+    li      $t0, 3          # temp = 3
+    sll     $t0, $t0, 4     # temp = 3 << 16
+    ori     $a1, $t0, 10    # 10 | 3 << 16
+    syscall                 # printInt(cnt, 10 | 3 << 16) //0x0003000A: decimal w/ 3 digits
+
+    li      $v0, 3          #
+    li      $a0, '\t'       #
+    syscall                 # putChar('\t') // Tab character
+
+    li      $v0, 6          #
+    move    $a0, $t1        #
+    li      $t0, 8          # temp = 8
+    sll     $t0, $t0, 4     # temp = 8 << 16
+    ori     $a1, $t0, 2     # 2 | 8 << 16
+    syscall                 # printInt(cnt, 2 | 8 << 16) //0x00080002: decimal w/ 8 digits
+
+    li      $a0, 5          #
+    jal     wait
+
+    li      $v0, 1          #
+    syscall                 # inkey()
+    move    $s2, $v0        # c = inkey()
+
+    bne     $s2, '+', endif # if (c == '+')
+    li      $s0, UP
+endif:
+    bne     $s2, '-', endif2 # if (c == '-')
+    li      $s0, DOWN
+endif2:
+
+    bne     $s0, UP, else   # if (state == UP)
+    addi    $s1, $s1, 1     # cnt = cnt + 1
+    andi    $s1, $s1, 0xFF  # (cnt + 1) & 0xFF  //Up counter MOD 256
+    j endif3
+else:
+    addi    $s1, $s1, -1    # cnt = cnt - 1
+    andi    #s1, $s1, 0xFF  # (cnt + 1) & OxFF  //Down counter MOD 256
+endif3:
+
+    bne     $s2, 'q', do    # while (c != 'q')
+
+    li      $v0, 0          # return 0
+
+    lb      $s0, 0($sp)
+    lb      $s1, 4($sp)
+    lb      $s2, 8($sp)
+    lb      $ra, 12($sp)
+    addi    $sp, $sp, 16
+    
+    jr      $ra
+
+##############################
+
+wait:
+    li      $t0, 0           # i = 0
+    li      $t1, 515000
+    mul     $t1, $t1, $a0    # 515000 * ts
+for:
+    bge     $t0, $t1, endfor    # while ( i < 515000)
+
+    addi    $t0, $t0, 1     # i++
+    j for
+endfor:
+    jr  $ra                  
+
+###############################
---
+++ 2 ano/2 Semestre/AC2/P/Guiao1/Extra/extra2.s
@@ -0,0 +1,178 @@
+#Mapa de registos
+#
+#
+#
+
+    .equ SIZE, 20
+    .data
+str1: .space 21
+str2: .space 21
+str3: .space 41
+
+msg1: .asciiz "Introduza 2 strings: "
+msg2: .asciiz "Resultados:\n"
+    .text
+    .globl main
+
+main:
+    addi    $sp, $sp, -4
+    sw      $ra, 0($sp)
+
+    li      $v0, 8              #
+    la      $a0, msg1           #
+    syscall                     # printStr("Introduza 2 strings: ");
+
+    li      $v0, 9              #
+    la      $a0, str1           #
+    li      $a1, SIZE           #
+    syscall                     # readStr( str1, SIZE )
+
+    li      $v0, 9              #
+    la      $a0, str2           #
+    li      $a1, SIZE           #
+    syscall                     # readStr( str2, SIZE )
+
+    li      $v0, 8              #
+    la      $a0, msg2           #
+    syscall                     # printStr("Resultados:\n");
+
+    la      $a0, str1           #
+    jal     strlen              # strlen(str1)
+
+    move    $a0, $v0            #
+    li      $v0, 6              #
+    li      $a1, 10             #
+    syscall                     # printInt( strlen(str1), 10 )
+
+    la      $a0, str2           #
+    jal     strlen              # strlen(str2)
+
+    move    $a0, $v0            #
+    li      $v0, 6              #
+    li      $a1, 10             #
+    syscall                     # printInt( strlen(str2), 10 )
+
+    la      $a0, str3           #
+    la      $a1, str1           #
+    jal     strcpy              # strcpy(str3, str1)
+
+    la      $a0, str3           #
+    la      $a1, str2           #
+    jal     strcat              # strcat(str3, str2)
+
+    move    $a0, $v0            #
+    li      $v0, 8              #
+    syscall                     # printStr( strcat(str3, str2) )
+
+    la      $a0, str1           #
+    la      $a1, str2           #
+    jal strcmp                  # strcmp(str1, str2)
+
+    move    $a0, $v0            #
+    li      $v0, 7
+    syscall                     # printInt10( str1, str2)
+
+    lw      $ra, 0($sp)
+    addi    $sp, $sp, 4
+
+    li      $v0, 0              # return 0
+
+    jr $ra
+
+##################################
+# Returns the length of string "str" (excluding the null character)
+#
+#   len : $t0
+strlen:
+    li      $t0, 0              # len = 0
+len_loop:
+    lb      $t1, 0($a0)         # *str
+    beq     $t1, 0, len_end      # while (*str != '\0')
+
+    addi    $a0, $a0, 1         # str++
+    addi    $t0, $t0, 1         # len++
+
+    j len_loop
+
+len_end:
+    move    $v0, $t0
+    jr      $ra
+
+####################################
+# Copy the string pointed by "src" (including the null character) to destination (pointed by "dst)
+#
+#   p = $t0
+
+strcpy:
+    move    $t0, $a0                # *p = dst
+copy_loop:
+    lb      $t1, 0($a1)             # $t1 = *src
+    sb      $t1, 0($a0)             # *dst = $t1
+    beq     $t1, 0, copy_end         # *dst != '\0'
+
+    addi    $a0, $a0, 1             # dst++
+    addi    $a1, $a1, 1             # src++
+
+    j copy_loop
+
+copy_end:
+    move    $v0, $t0                # return p
+    jr      $ra
+
+#####################################    
+# Concatenates "dst" and "src" string
+# The result is stored in the "dst" string
+#
+#   $s0 = p
+#   $s1 = $a0 (dst)
+#   $s2 = $a1 (src)
+
+strcat:
+    addi    $sp, $sp, -4
+    sw      $ra, 0($sp)
+
+    move    $s1, $a0
+    move    $s2, $a1
+    move    $s0, $s1
+
+cat_loop:
+    lb      $t0, 0($s1)             # *dst
+    beq     $t0, 0, cat_end         # *dst != '\0'
+
+    addi    $s1, $s1, 1             # dst++
+    j cat_loop
+cat_end:
+    move    $a0, $s1                #
+    move    $a1, $s2                #
+    jal strcpy                      # strcpy( dst, src)
+
+    move    $v0, $s0                # return p
+
+    lw      $ra, 0($sp)
+    addi    $sp, $sp, 4
+    jr $ra
+
+######################################
+# Compares two strings, character by character 
+# Returned value is: 
+#  < 0   string "str1" is "less than" string "str2" (first  
+#    non-matching character in str1 is lower, in ASCII, than  
+#    that of str2) 
+#  = 0  string "str1" is equal to string "str2" 
+#  > 0  string "str1" is "greater than" string "str2" (first  
+#    non-matching character in str1 is greater, in ASCII, than  
+#    that of str2)
+
+strcmp:
+cmp_copy:
+    lb      $t0, 0($a0)             # *str1
+    lb      $t1, 0($a1)             # *str2
+    bne     $t0, $t1, cmp_end       # if (*str1 != *str2) break
+    beq     $t0, 0, cmp_end         # if (*str1 = '\0') break
+
+    addi    $a0, $a0, 1             # str1++
+    addi    $a1, $a1, 1             # str2++
+    j cmp_copy
+cmp_end:
+    sub     $v0, $t0, $t1
+    jr  $ra
